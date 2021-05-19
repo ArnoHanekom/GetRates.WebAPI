4fc229d320edee0878682e1f4c4bb88fbd4d12e9
Arno <arno@soms-software.com>
2021-05-19 12:37:25 +0200

    Add project files.

---
+++ Controllers/CryptoController.cs
@@ -0,0 +1,37 @@
+﻿using GetRates.DAL.Data;
+using GetRates.DAL.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GetRates.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CryptoController : ControllerBase
+    {
+        private readonly IUnitOfWork _IUnitOfWork;
+        public CryptoController(IUnitOfWork IUnitOfWork)
+        {
+            _IUnitOfWork = IUnitOfWork;
+        }
+        
+        [HttpGet]
+        [Route("latest/{converts}")]
+        public async Task<List<Crypto>> GetLatestListingsAsync(string converts)
+        {
+            List<string> convertList = converts.Split(',').ToList();
+            await _IUnitOfWork.Cryptos.GetLatestListingsAsync(convertList);
+
+            return await _IUnitOfWork.Cryptos.GetAllFromDbAsync();
+        }
+
+        [HttpGet]
+        [Route("listings")]
+        public async Task<List<Crypto>> GetDbListings()
+        {             
+            return await _IUnitOfWork.Cryptos.GetAllFromDbAsync();
+        }
+    }
+}
